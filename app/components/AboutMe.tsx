ae97003a84ee9809122ff71dcbe7153cb66bee1b
abirwan <abi.rahmawan01@student.uns.ac.id>
2024-12-25 18:59:05 +0700

    update style

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import MyPic from "../assets/images/me.png";
@@ -8,27 +9,71 @@ import { BiLogoTypescript } from "react-icons/bi";
 const AboutMe = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row md:justify-between md:space-x-8 items-center px-8 py-8 mt-8">
-        <div className="w-full md:w-1/2">
-          <Image src={MyPic} className="rounded-2xl" alt="self"></Image>
-        </div>
-        <div className="space-y-3 justify-center md:space-y-6 mt-8 md:mt-0 text-center md:text-start w-full md:w-1/2"> 
-          <h1 className="text-5xl md:text-7xl font-black" style={{ fontFamily: "var(--font-unbounded)" }}>
-            About Me
-          </h1>
-          <p className="text-2xl font-bold">Who is Abi Rahmawan?</p>
-          <p className='text-xl'>Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
-          <div className="flex justify-center md:justify-start space-x-2 md:space-x-4">
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><CgFigma/></div>
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><RiJavascriptFill /></div>
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><BiLogoTypescript /></div>
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><RiReactjsFill /></div>
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><RiNextjsFill /></div>
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><RiTailwindCssFill /></div>
-            <div className="p-2 md:p-4 text-2xl md:text-4xl bg-yellow-500 rounded-full"><RiBootstrapFill /></div>
+      {/* <div className="relative">
+        <div className="bg-text text-12xl text-zinc-950/5 font-black absolute flex justify-center items-center text-center inset-x-0 bottom-40 z-0 pointer-events-none" style={{ fontFamily: "var(--font-unbounded)" }}>
+          AboutMe
+        </div> */}
+        <div className="relative z-1 flex flex-col md:flex-row md:justify-between md:space-x-8 items-center px-4 md:px-8 py-8 mt-8">
+          <div className="w-full md:w-1/2">
+            <Image src={MyPic} className="rounded-2xl" alt="self"></Image>
+          </div>
+          <div className="space-y-3 justify-center md:space-y-6 mt-8 md:mt-0  w-full md:w-1/2">
+            <h1 className="text-5xl md:text-7xl font-black" style={{ fontFamily: "var(--font-unbounded)" }}>
+              About Me
+            </h1>
+            <p className="text-2xl font-bold">Who is Abi Rahmawan?</p>
+            <p className="text-xl">Rorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
+            <div className="w-full inline-flex flex-nowrap space-x-4 overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%) group hover:animation-paused">
+              <div className="flex justify-center md:justify-start space-x-2 md:space-x-4 animate-infinite-scroll group-hover:animation-paused">
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <CgFigma />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiJavascriptFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <BiLogoTypescript />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiReactjsFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiNextjsFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiTailwindCssFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiBootstrapFill />
+                </div>
+              </div>
+              <div className="flex justify-center md:justify-start space-x-2 md:space-x-4 animate-infinite-scroll group-hover:animation-paused">
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <CgFigma />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiJavascriptFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <BiLogoTypescript />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiReactjsFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiNextjsFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiTailwindCssFill />
+                </div>
+                <div className="p-4 text-3xl md:text-4xl bg-yellow-500 rounded-full">
+                  <RiBootstrapFill />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      {/* </div> */}
     </>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,40 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize:{
+        '12xl': '20rem'
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      zIndex: {
+        '-1': '-1',
+      },
+      top: {
+        '-1': '-8rem'
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: any) {
+      addUtilities({
+        '.animation-paused': {
+          'animation-play-state': 'paused',
+        },
+        '.animation-running': {
+          'animation-play-state': 'running',
+        },
+      });
+    },
+  ],
 } satisfies Config;
