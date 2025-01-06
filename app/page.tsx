062d89c185804dee996094b8230ddb0479158c83
abirwan <abi.rahmawan01@student.uns.ac.id>
2025-01-06 10:00:56 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
     <>
       <Navbar />
 
-      <section id="home" className="mt-12 md:mt-24">
+      <section id="home" className="mt-12 lg:mt-24">
         <Hero />
       </section>
 
@@ -35,49 +35,55 @@ export default function Home() {
         <ServiceCard />
       </section>
 
-      <section id="about" className="mt-12 md:mt-24">
+      <section id="about" className="mt-12 lg:mt-24">
         <AboutMe />
         <div className="w-full px-4 md:px-8">
-          <div className="grid grid-col-1 md:grid-cols-2 grid-row-2 md:grid-rows-1 gap-6 mt-8">
+          <div className="grid grid-col-1 lg:grid-cols-2 grid-row-2 lg:grid-rows-1 gap-6 mt-8">
             <AccordionCard title="Education">
-              <Accordion item="Universitas Sebelas Maret" year="2021-2024" content="D3 Teknik Informatika | GPA: 3.87/4.00" />
+              <Accordion 
+                item="Universitas Sebelas Maret" 
+                year="2021-2024" 
+                content="D3 Teknik Informatika | GPA: 3.87/4.00" />
             </AccordionCard>
             <AccordionCard title="Work Experience">
-              <Accordion item="PT. Amanin Internasional" year="2023" content="Senior High Vocational School" />
+              <Accordion 
+                item="PT. Amanin Internasional" 
+                year="2023" 
+                content="I designed a comprehensive web dashboard for managing menus and analyzing facial expressions, built interactive UI components with React.js to enhance user experience, and implemented real-time facial detection using TensorFlow.js. I also created and integrated APIs to connect the facial recognition system with the database, ensuring seamless data flow, while compiling detailed technical documentation to support system implementation and maintenance." />
             </AccordionCard>
           </div>
         </div>
       </section>
 
-      <section id="project" className="mt-12 md:mt-24">
+      <section id="project" className="mt-12 lg:mt-24">
         <ProjectTabs />
       </section>
 
-      <section id="contact" className="mt-12 md:mt-24">
+      <section id="contact" className="mt-12 lg:mt-24">
         <Contact />
       </section>
 
-      <div className="mt-12 md:mt-24 px-4 md:px-8">
+      <div className="mt-12 lg:mt-24 px-4 md:px-8">
         <h1 className="text-5xl md:text-7xl font-black mb-8 text-start" style={{ fontFamily: "var(--font-unbounded)" }}>
           FAQ
         </h1>
         <div className="space-y-4">
           <Faq
             question="Are you available for freelance work?"
-            answer="Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+            answer="Yes, I am available for freelance work and would be happy to collaborate on exciting projects. Feel free to share the details of your project, and we can discuss how I can contribute to bringing your ideas to life."
           />
           <Faq
             question="May i download your CV/Resume for information?"
-            answer="Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+            answer="Sure! You can download my CV/Resume directly from my website. You can access it anytime"
           />
           <Faq
             question="How do i navigate through your portofolio project?"
-            answer="Sorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+            answer="Head over to the Projects section to see a collection of my works. Each project is displayed with a title, description, and preview."
           />
         </div>
       </div>
 
-      <div className="bg-zinc-950 mt-12 md:mt-24">
+      <div className="bg-zinc-950 mt-12 lg:mt-24">
         <Footer />
       </div>
     </>
